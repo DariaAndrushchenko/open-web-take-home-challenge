8e4c23d8e2590904516a2f4be3aa9024982dd70e
Daria <52241327+DariaAndrushchenko@users.noreply.github.com>
2024-02-25 21:25:22 +0200

    add global shared actions

---
+++ features/step-definitions/global.ts
@@ -0,0 +1,18 @@
+
+import { Given, BeforeAll } from '@wdio/cucumber-framework';
+import { driver, $ } from '@wdio/globals'
+import { WELCOME_SCREEN_IDS } from '../../const';
+
+const PASSWORD = 'OpenWebQA'
+
+BeforeAll(async () => {
+    await driver.execute('mobile: terminateApp', { bundleId: 'com.openweb.AutomationInterview' })
+    await driver.execute('mobile: launchApp', { bundleId: 'com.openweb.AutomationInterview' })
+})
+
+Given("I'm logged in", async () => {
+    const authInput = await $(WELCOME_SCREEN_IDS.passwordTextFieldIdentifier)
+    await authInput.addValue(PASSWORD)
+    const authButton = await $(WELCOME_SCREEN_IDS.autenticateBtnIdentifier)
+    await authButton.click()
+});
