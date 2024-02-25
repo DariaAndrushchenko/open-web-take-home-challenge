6c21f1b054e7b8b081cd5d035c43205bb65b8aad
Daria <52241327+DariaAndrushchenko@users.noreply.github.com>
2024-02-25 21:25:06 +0200

    Setup appium and wdio

---
+++ const.ts
@@ -0,0 +1,25 @@
+
+export const WELCOME_SCREEN_IDS = {
+    descriptionLblIdentifier: "~description_label_id",
+    enterPasswordLblIdentifier: "~enter_password_label_id",
+    passwordTextFieldIdentifier: "~password_text_field_id",
+    autenticateBtnIdentifier: "~autenticate_button_id",
+    allRightsLblIdentifier: "~all_rights_label_id",
+
+}
+export const TESTS_SCREEN_IDS = {
+
+descriptionLblIdentifier : "~description_label_id",
+secretWordTestButtonIdentifier : "~secret_word_test_btton_id",
+animationTestButtonIdentifier : "~animation_test_button_id",
+    }
+
+export const SECRET_WORD_SCREEN_IDS = {
+    descriptionLblIdentifier: "~description_label_id",
+    secretWordIdentifier: "~secret_word_label_id"
+}
+
+export const ANIMATION_SCREEN_IDS = {
+    descriptionLblIdentifier: "~text_to_find_label_id",
+    welcomeIdentifier: "~welcome_label_id",
+}
